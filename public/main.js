6fb0ccc77b962515195649cde0a790439b10fd8b
Amir Djavaherian <amir.djavaherian@nestio.com>
2016-07-08 16:35:55 -0400

    updating the fe js

--- public/main.js
+++ public/main.js
@@ -10,12 +10,17 @@ $(function() {
     socket.on('new message', function (data) {
         // console.debug('tweet', data.message);
         var $tweet = $('<h1>', {
-            class : "tweet-h1"
+            class : "tweet-h1",
+            css: {'color': '#' + Math.floor(Math.random()*16777215).toString(16)},
+            html: data.message
         }); // tweet h1
 
-        $tweet.html(data.message).css({'color': '#'+Math.floor(Math.random()*16777215).toString(16)}).appendTo($tweetDiv);
+        $tweet.html(data.message).appendTo($tweetDiv);
 
     });
 
+    setInterval(function(){
+        $tweetDiv.empty();
+    }, 60000);
 
 });
